272c71aeff90eb543ac61d29807dff045c99b0a8
Timur Badretdinov <destinerx@gmail.com>
2019-10-26 21:26:18 +0300

    Basic UI

--- public/views/Main.vue
+++ public/views/Main.vue
@@ -1,6 +1,17 @@
 <template>
 	<div>
 		<h1>OrFeed</h1>
+		<div id="params">
+			<div id="input">
+				<input v-model="inputAsset">
+				<input v-model="inputAmount">
+			</div>
+			<div id="output">
+				<input v-model="outputAsset">
+				<span>{{ outputAmount }}</span>
+			</div>
+			
+		</div>
 	</div>
 </template>
 
@@ -11,10 +22,29 @@ const infuraId = '202f7e63f7924de1b214ec587219d93d';
 const provider = new ethers.providers.InfuraProvider('homestead', infuraId);
 
 export default {
-
+	data() {
+		return {
+			inputAsset: 'JPY',
+			outputAsset: 'USD',
+			inputAmount: '100000',
+			outputAmount: '0',
+		}
+	},
 }
 </script>
 
 <style scoped>
-	
+#params {
+	display: flex;
+}
+
+#input,
+#output {
+	display: flex;
+	flex-direction: column;
+}
+
+#output {
+	margin-left: 2em;
+}
 </style>
