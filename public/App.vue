e13de7d0b2eb94a5e13dcb51abd988c2124f7832
Timur Badretdinov <destinerx@gmail.com>
2019-10-26 21:09:39 +0300

    Initial commit

---
+++ public/App.vue
@@ -0,0 +1,32 @@
+<template>
+	<div id="app">
+		<div id="left">
+		</div>
+		<div id="center">
+			<div class="nav-bar">
+				<div>
+					<div class="nav-option">
+						<router-link to="/">Main</router-link>
+					</div>
+				</div>
+			</div>
+			<br>
+			<router-view/>
+		</div>
+		<div id="right">
+		</div>
+	</div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto');
+
+/*==================================================
+  Basics
+  ==================================================*/
+
+body {
+	font-family: 'Roboto', -apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', 'Helvetica Neue', Arial, sans-serif;
+	font-size: 18px;
+}
+</style>
