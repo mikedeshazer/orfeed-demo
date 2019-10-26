979403c7c691cf69b4411b3764c7bb244a00fbbd
Timur Badretdinov <destinerx@gmail.com>
2019-10-26 21:19:35 +0300

    Add Infura provider

--- public/views/Main.vue
+++ public/views/Main.vue
@@ -4,6 +4,17 @@
 	</div>
 </template>
 
+<script>
+import { ethers } from 'ethers';
+
+const infuraId = '202f7e63f7924de1b214ec587219d93d';
+const provider = new ethers.providers.InfuraProvider('homestead', infuraId);
+
+export default {
+
+}
+</script>
+
 <style scoped>
 	
 </style>
